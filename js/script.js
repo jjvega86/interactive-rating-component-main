7490463ed71ea1149cc58de53a539901e5bb5d71
JJ <jj.vega86@gmail.com>
2022-11-07 06:36:24 -0500

    📦 NEW: MVP on required features

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-console.log("Hello World");
+// TODO: Add event listeners and events for choosing rating and submitting
+
+let ratings = document.getElementsByClassName("rating");
+let submitBtn = document.querySelector(".submit-rating");
+
+function setActiveState(e) {
+  removeActiveStates();
+  let rating = document.querySelector(`#${e.target.id}`);
+  rating.className += " rating-active";
+}
+
+function removeActiveStates() {
+  let updatedRatings = document.getElementsByClassName(`rating`);
+  for (let i = 0; i < updatedRatings.length; i++) {
+    if (updatedRatings[i].className.includes("rating-active")) {
+      updatedRatings[i].className = "rating circular-bg";
+      break;
+    }
+  }
+}
+
+function submitRating(e) {
+  let ratingState = document.querySelector(".rating-state");
+  let thankYouState = document.querySelector(".thank-you-state");
+  let finalRating = document.querySelector(".rating-active");
+  if (!finalRating) return;
+  let ratingPill = document.querySelector("#final-rating");
+  ratingPill.textContent = finalRating.textContent;
+  ratingState.style.display = "none";
+  thankYouState.style.display = "block";
+}
+
+for (let i = 0; i < ratings.length; i++) {
+  ratings[i].addEventListener("click", setActiveState);
+}
+
+submitBtn.addEventListener("click", submitRating);
